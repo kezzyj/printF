db04aec5230249280b78c7c8ad6493629716277f
kezzyj <karachidavid31@gmail.com>
2022-05-25 05:00:38 -0700

    fixing printf

--- _printf.c
+++ _printf.c
@@ -1,93 +1,51 @@
 #include "main.h"
 
 /**
- *
- *  * count_one_bits - Counts the number of bits set
- *
- *   *                  to one in a binary number.
- *
- *    * @num: The binary number.
- *
- *     *
- *
- *      * Return: The number of bits set to one.
- *
- *       */
+ * count_one_bits - Counts the number of bits set
+ * to one in a binary number.
+ * @num: The binary number.
+ * Return: The number of bits set to one.
+ */
 
 unsigned char count_one_bits(unsigned char num)
-
 {
-
 	unsigned char count = 0;
 
-
-
 	while (num != 0)
-
 	{
-
 		if ((num & 1) == 1)
 
 			count++;
 
 		num >>= 1;
-
 	}
-
-
-
 	return (count);
-
 }
 
-
-
-
-
 /**
- *
- *  * cleanup - Peforms cleanup operations for _printf.
- *
- *   * @args: A va_list of arguments provided to _printf.
- *
- *    * @output: A buffer_t struct.
- *
- *     */
+* cleanup - Peforms cleanup operations for _printf.
+* @args: A va_list of arguments provided to _printf.
+* @output: A buffer_t struct.
+*/
 
 void cleanup(va_list args, buffer_t *output)
-
 {
-
 	va_end(args);
 
 	write(1, output->start, output->len);
 
 	free_buffer(output);
-
 }
 
-
-
-
-
 /**
- *
- *  * run_printf - Reads through the format string for _printf.
- *
- *   * @format: Character string to print - may contain directives.
- *
- *    * @output: A buffer_t struct containing a buffer.
- *
- *     * @args: A va_list of arguments.
- *
- *      *
- *
- *       * Return: The number of characters stored to output.
- *
- *        */
+* run_printf - Reads through the format string for _printf.
+* @format: Character string to print - may contain directives
+* @output: A buffer_t struct containing a buffer.
+* @args: A va_list of arguments.
+* Return: The number of characters stored to output.
+*/
 
 int run_printf(const char *format, va_list args, buffer_t *output)
-
 {
 
 	int i, ret = 0;
@@ -96,22 +54,15 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 
 	unsigned char flag, len;
 
-	unsigned int (*f)(va_list, buffer_t *,\
-
-			unsigned char, char, char, unsigned char);
-
-
+	unsigned int (*f)(va_list, buffer_t *, unsigned char, char, char, unsigned char);
 
 	for (i = 0; *(format + i); i++)
-
 	{
 
 		len = 0;
 
 		if (*(format + i) == '%')
-
 		{
-
 			flag = handle_flags(format + i + 1);
 
 			tmp = count_one_bits(flag);
@@ -119,7 +70,6 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 			wid = handle_width(args, format + i + tmp + 1, &tmp);
 
 			prec = handle_precision(args, format + i + tmp + 1, &tmp);
-
 			len = handle_length(format + i + tmp + 1);
 
 			tmp += (len != 0) ? 1 : 0;
@@ -127,7 +77,6 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 			f = handle_specifiers(format + i + tmp + 1);
 
 			if (f != NULL)
-
 			{
 
 				i += tmp + 1;
@@ -139,9 +88,7 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 			}
 
 			else if (*(format + i + tmp + 1) == '\0')
-
 			{
-
 				ret = -1;
 
 				break;
@@ -165,28 +112,16 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 
 
 /**
- *
- *  * _printf - Outputs a formatted string.
- *
- *   * @format: Character string to print - may contain directives.
- *
- *    *
- *
- *     * Return: The number of characters printed.
- *
- *      */
+* _printf - Outputs a formatted string.
+* @format: Character string to print - may contain directives.
+* Return: The number of characters printed.
+*/
 
 int _printf(const char *format, ...)
-
 {
-
 	buffer_t *output;
-
 	va_list args;
-
 	int ret;
-
-
 
 	if (format == NULL)
 
@@ -198,16 +133,9 @@ int _printf(const char *format, ...)
 
 		return (-1);
 
-
-
 	va_start(args, format);
 
-
-
 	ret = run_printf(format, args, output);
-
-
-
 	return (ret);
 
 }
